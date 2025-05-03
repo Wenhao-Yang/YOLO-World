2ba3204448a558e8ebef4f980ba22ad5114eb208
wenhao-mac <874681044@qq.com>
2025-05-03 17:36:40 +0800

    [config] yoss-s

--- configs/pretrain/YOSS_S.py
+++ configs/pretrain/YOSS_S.py
@@ -16,13 +16,14 @@ base_lr = 1e-3
 weight_decay = 0.025
 train_batch_size_per_gpu = 8
 val_batch_size_per_gpu   = 1
+
 # load_from = 'work_dirs/yolo_world_v2_dataset_audio/pretrained.pth'
 # load_from = 'pretrained/audio_ckp.pth'
 load_from = 'pretrained/audio_ckp_mix.pth'
 
 # img_scale = (1280, 1280)
 img_scale = _base_.img_scale
-model_fp = '/home/yangwenhao/project/SpeechCLIP/exp/coco/relate0/epoch=22-step=49999-val_recall_mean_10=76.5962.ckpt'
+audio_model_fp = './SpeechCLIP/exp/coco/relate0/epoch=22-step=49999-val_recall_mean_10=76.5962.ckpt'
 
 # model settings
 model = dict(
@@ -35,7 +36,7 @@ model = dict(
         _delete_=True,
         type='MultiModalAudioYOLOBackbone',
         image_model={{_base_.model.backbone}},
-        audio_model=model_fp,
+        audio_model=audio_model_fp,
         audio_blank='demo/audio_blank.npy',
         frozen_stages=4,
         freeze_audio=1,
@@ -65,14 +66,6 @@ text_transform = [
          meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'flip', 'audio',
                     'flip_direction', 'texts'))
 ]
-# text_transform = [
-#     *_base_.test_pipeline[:-1],
-#     dict(type='LoadText'),
-#     dict(type='mmdet.PackDetInputs',
-#          meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'flip', 'audio',
-#                     'flip_direction', 'texts'))
-# ]
-
 
 audio_transform = [
     dict(type='LoadAudio',
@@ -83,7 +76,6 @@ train_pipeline = [
     *_base_.pre_transform,
     dict(type='MultiModalMosaic',
          img_scale=img_scale,
-         # center_ratio_range=(0.25, 0.75),
          pad_val=114.0,
          pre_transform=_base_.pre_transform),
     dict(
@@ -123,57 +115,44 @@ train_pipeline_stage2 = [
 flickr_train_dataset = dict(
     type='YOLOv5MixedGroundingAudioDataset',
     data_root='data/flickr_audio_tmp/',
-    # ann_file='annotations/final_flickr_separateGT_train_8k2.json',
     ann_file='annotations/final_flickr_separateGT_train_8k_scores_norm.json',
-    # ann_file='annotations/final_flickr_separateGT_train_32k3.json',
     data_prefix=dict(img='images/'),
     filter_cfg=dict(filter_empty_gt=True, min_size=32),
     pipeline=train_pipeline)
 
+
 # coco_train_dataset = dict(
 #     type='YOLOv5MixedGroundingAudioDataset',
-#     data_root='data/coco2014/',
+#     data_root='data/coco2014_caps/',
 #     # ann_file='annotations/final_flickr_separateGT_train_8k2.json',
-#     ann_file='annotations/final_mixed_train_8k_norm_scores.json',
+#     ann_file='annotations/final_mixed_train_8k_caps.json',
 #     # ann_file='annotations/final_flickr_separateGT_train_32k3.json',
 #     data_prefix=dict(img='images/'),
 #     filter_cfg=dict(filter_empty_gt=True, min_size=32),
 #     pipeline=train_pipeline)
 
-coco_train_dataset = dict(
-    type='YOLOv5MixedGroundingAudioDataset',
-    data_root='data/coco2014_caps/',
-    # ann_file='annotations/final_flickr_separateGT_train_8k2.json',
-    ann_file='annotations/final_mixed_train_8k_caps.json',
-    # ann_file='annotations/final_flickr_separateGT_train_32k3.json',
-    data_prefix=dict(img='images/'),
-    filter_cfg=dict(filter_empty_gt=True, min_size=32),
-    pipeline=train_pipeline)
 
-
-gqa_train_dataset = dict(
-    type='YOLOv5MixedGroundingAudioDataset',
-    data_root='data/GQA/',
-    # ann_file='annotations/final_mixed_train_8k.json',
-    ann_file='annotations/final_mixed_train_17k_norm_scores.json',
-    # ann_file='annotations/final_flickr_separateGT_train_1.json',
-    data_prefix=dict(img='images/'),
-    filter_cfg=dict(filter_empty_gt=True, min_size=32),
-    pipeline=train_pipeline)
+# gqa_train_dataset = dict(
+#     type='YOLOv5MixedGroundingAudioDataset',
+#     data_root='data/GQA/',
+#     # ann_file='annotations/final_mixed_train_8k.json',
+#     ann_file='annotations/final_mixed_train_17k_norm_scores.json',
+#     # ann_file='annotations/final_flickr_separateGT_train_1.json',
+#     data_prefix=dict(img='images/'),
+#     filter_cfg=dict(filter_empty_gt=True, min_size=32),
+#     pipeline=train_pipeline)
 
 train_dataloader = dict(batch_size=train_batch_size_per_gpu,
                         collate_fn=dict(type='yolow_collate'),
                         dataset=dict(_delete_=True,
                                      type='ConcatDataset',
                                      datasets=[
-                                         # flickr_train_dataset,
-                                         coco_train_dataset,
+                                         flickr_train_dataset,
+                                        #  coco_train_dataset,
                                          # gqa_train_dataset
                                      ],
                                      ignore_keys=['classes', 'palette']))
 test_audio_transform = [
-    # dict(type='LoadAudio',
-    #     audio_path='data/audios/lvis_v1_class_audio.json'),
     dict(type='LoadRandomAudio',
          # audio_path='data/audios/lvis_v1_class_audio2.json',
          audio_path='data/audios/coco_class_audio.json',
@@ -198,73 +177,6 @@ test_pipeline = [
                     'scale_factor', 'pad_param', 'texts'))
 ]
 
-# coco_val_dataset = dict(
-#     _delete_=True,
-#     type='MultiModalDataset',
-#     dataset=dict(type='YOLOv5LVISV1Dataset',
-#                  data_root='data/coco/',
-#                  test_mode=True,
-#                  ann_file='lvis/lvis_v1_minival_inserted_image_name.json',
-#                  data_prefix=dict(img=''),
-#                  batch_shapes_cfg=None),
-#     class_text_path='data/texts/lvis_v1_class_texts.json',
-#     pipeline=test_pipeline)
-
-# flickr_val_dataset = dict(
-#     _delete_=True,
-#     type='MultiModalDataset',
-#     dataset=dict(type='YOLOv5MixedGroundingAudioDataset',
-#                  data_root='data/flickr_audio_tmp/',
-#                  test_mode=True,
-#                  ann_file='annotations/final_flickr_separateGT_val.json',
-#                  data_prefix=dict(img='images/'),
-#                  batch_shapes_cfg=None),
-#     # class_text_path='data/texts/lvis_v1_class_texts.json',
-#     pipeline=test_pipeline)
-
-# flickr_val_dataset = dict(
-#     type='YOLOv5MixedGroundingAudioDataset',
-#     data_root='data/flickr_audio_tmp/',
-#     ann_file='annotations/final_flickr_separateGT_val.json',
-#     data_prefix=dict(img='images/'),
-#     filter_cfg=dict(filter_empty_gt=True, min_size=32),
-#     pipeline=test_pipeline)
-
-# val_dataloader = dict(dataset=coco_val_dataset,
-#                      batch_size=val_batch_size_per_gpu,)#,collate_fn=dict(type='predict_collate')) #collate_fn=dict(type='yolow_collate'),
-# collate_fn=dict(type='predict_collate'),
-# val_dataloader = dict(dataset=flickr_val_dataset) #collate_fn=dict(type='yolow_collate'),
-
-# test_dataloader = val_dataloader
-
-# val_evaluator = dict(type='mmdet.LVISMetric',
-#                      ann_file='data/coco/lvis/lvis_v1_minival_inserted_image_name.json',
-#                      metric='bbox')
-# val_evaluator = dict(type='mmdet.LVISMetric',
-#                      ann_file='data/flickr_audio_tmp/annotations/final_flickr_separateGT_val.json',
-#                      metric='bbox')
-
-# test_pipeline = [
-#     *_base_.test_pipeline[:-1],
-#     dict(type='LoadText'),
-#     *test_audio_transform,
-#     dict(type='mmdet.PackDetInputs',
-#          meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
-#                     'audio',
-#                     'scale_factor', 'pad_param', 'texts'))
-# ]
-
-# coco_val_dataset = dict(
-#     _delete_=True,
-#     type='MultiModalDataset',
-#     dataset=dict(type='YOLOv5LVISV1Dataset',
-#                  data_root='data/coco/',
-#                  test_mode=True,
-#                  ann_file='lvis/lvis_v1_val_sub512.json',
-#                  data_prefix=dict(img=''),
-#                  batch_shapes_cfg=None),
-#     class_text_path='data/texts/lvis_v1_class_texts.json',
-#     pipeline=test_pipeline)
 
 coco_val_dataset = dict(
     _delete_=True,
@@ -278,26 +190,10 @@ coco_val_dataset = dict(
     class_text_path='data/texts/coco_class_texts.json',
     pipeline=test_pipeline)
 
-# coco_val_dataset = dict(
-#     _delete_=True,
-#     type='MultiModalDataset',
-#     dataset=dict(type='YOLOv5LVISV1Dataset',
-#                  data_root='data/coco/',
-#                  test_mode=True,
-#                  ann_file='lvis/lvis_v1_minival_inserted_image_name.json',
-#                  data_prefix=dict(img=''),
-#                  batch_shapes_cfg=None),
-#     class_text_path='data/texts/lvis_v1_class_texts.json',
-#     pipeline=test_pipeline)
-
 
 val_dataloader = dict(dataset=coco_val_dataset,
                      batch_size=val_batch_size_per_gpu,)
 test_dataloader = val_dataloader
-
-# val_evaluator = dict(type='mmdet.LVISMetric',
-#                      ann_file='data/coco/lvis/lvis_v1_val_sub512.json',
-#                      metric='bbox')
 
 val_evaluator = dict(type='mmdet.LVISMetric',
                      ann_file='data/coco2014/annotations/instances_val2014_neg_2k.json',
@@ -334,8 +230,6 @@ optim_wrapper = dict(optimizer=dict(
     paramwise_cfg=dict(bias_decay_mult=0.0,
                         norm_decay_mult=0.0,
                         custom_keys={
-                            # 'backbone':
-                            # dict(lr_mult=0.0),
                             'backbone.image_model':
                             dict(lr_mult=0.0),
                             'backbone.audio_model':
